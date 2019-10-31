ef76cd8437b8271c2a5ad1ad3973ba7541c0940b
Carl Månsson <carl.mnssn@gmail.copm>
2019-10-31 22:54:09 +0100

    add better state management

--- SERVER/index.js
+++ SERVER/index.js
@@ -2,13 +2,31 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 8000;
+const bodyParser = require('body-parser');
+
+const savedPatterns = [];
 
 
-app.use('/sounds', cors(), express.static('assets'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/hello', cors(), (req, res) => {
   res.send('YES, I\'m awake!!')
 })
+
+app.get('/sounds/:sound', cors(), (req, res) => {
+  res
+    .sendfile('./assets/' + req.params.sound)
+})
+
+app.post('/pattern', cors(), (req, res) => {
+
+})
+
+app.get('/pattern')
+
+app.get('/pattern:id')
 
 app.listen(port, () => console.log(`Sound API up and running on port ${port}!`))
 
